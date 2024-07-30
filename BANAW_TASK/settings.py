00566099c5367b50c1884f75462f03ca2a8afaf7
mdarafatyeasin <yeasinarafat54239@gmail.com>
2024-07-31 03:01:07 +0600

    update cors

--- BANAW_TASK/settings.py
+++ BANAW_TASK/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-htdv9j6)4pr9n(x+$xs%lu39j5oyq%!97e=i$jnw)7g@q@%oql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework.authtoken',
     'rest_framework',
     'authentication',
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
