974cd57fae48e80e1f0994b54a8b74cede538ccc
mdarafatyeasin <yeasinarafat54239@gmail.com>
2024-07-31 04:01:39 +0600

    cors

--- BANAW_TASK/settings.py
+++ BANAW_TASK/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
